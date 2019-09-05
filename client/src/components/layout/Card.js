3b93c6e7ccbee15d3a886487cc4509a49536822a
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-09-04 22:59:11 -0400

    added more styling to cards + link

--- client/src/components/layout/Card.js
+++ client/src/components/layout/Card.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const Card = () => {
+const Card = props => {
+  const { user } = props;
+  console.log('props: ', props)
   return(
-    <h1>Card</h1>
+    <NavLink to='/'>
+      <div className="card">
+        <div className="card__photo"><i className="fas fa-user fa-5x"></i></div>
+        <h1 className="card__header">{user.username}</h1>
+          <div className="card__actions">
+            <div className="card__button"><i className="fab fa-linkedin-in fa-2x"></i></div>
+            <div className="card__button"><i className="fab fa-github fa-2x"></i></div>
+          </div>
+      </div>
+    </NavLink>
   );
 };
 
--- client/src/components/pages/Candidates.js
+++ client/src/components/pages/Candidates.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_CANDIDATES } from '../../queries/queries';
+import Card from '../layout/Card';
 
 const Register = () => {
   const { loading, data } = useQuery(GET_CANDIDATES);
   const { users } = data;
 
-  console.log(users)
+  const renderList = () => {
+    return users.map(user => <Card user={user} key={user.id}/>)
+  };
   
   if(loading) {
     return <h1>Loading...</h1>
@@ -15,17 +18,10 @@ const Register = () => {
   return(
     <div className="container">
       <div className="candidates">
-        <h1>The candidates</h1>
-
-        <div className="card">
-          <div className="card__photo"><i class="fas fa-user fa-5x"></i></div>
-          <h1 className="card__header">Ryaaanandrew</h1>
-            <div className="card__actions">
-              <div className="card__button"><i class="fab fa-linkedin-in fa-2x"></i></div>
-              <div className="card__button"><i class="fab fa-github fa-2x"></i></div>
-            </div>
+        <h1 className="candidates__header">Meet your future developers</h1>
+        <div className="candidates__list">
+          {renderList()}
         </div>
-
       </div>
     </div>
   );
