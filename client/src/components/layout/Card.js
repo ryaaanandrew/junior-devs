c40fbe1b6e6a362f3d9adc57d8263ce76dcb06f6
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-09-11 23:42:34 -0400

    added skills to candidate card

--- client/src/components/layout/Card.js
+++ client/src/components/layout/Card.js
@@ -9,31 +9,37 @@ const Card = props => {
   };
 
   return(
-    <NavLink to={`/user/${user.id}`}>
+    
       <div className="card">
         <div className="card__left" style={backgroundStyle}><div className="card__overlay"></div></div>
         <div className="card__right">
-          <h3>{user.username}</h3>
+          <NavLink to={`/user/${user.id}`}>
+            <h3>{user.username}</h3>
+          </NavLink>
+          <ul className="card__skills">
+              { user.skills && user.skills.slice(0,3).map((skill, i) => {
+                return <li key={i}>{skill}</li>
+              }) }
+          </ul>
           <div className="card__actions">
-            <a href={`http://${ user.linkedIn }`}>
-              <div className="card__button card__button--linkedin">
+            <div className="card__button card__button--linkedin">
+              <a href={`http://${ user.linkedIn }`}>
                 <i className="fab fa-linkedin-in fa-3x"></i>
-              </div>
-            </a>
-            <a href={`https://${ user.github }`}>
-              <div className="card__button card__button--github">
+              </a>
+            </div>
+            <div className="card__button card__button--github">
+              <a href={`https://${ user.github }`}>
                 <i className="fab fa-github fa-3x"></i>
-              </div>
-            </a>
-            <a href={`https://${ user.github }`}>
-              <div className="card__button card__button--email">
-              <i class="fas fa-envelope fa-3x"></i>
-              </div>
-            </a>
+              </a>
+            </div>
+            <div className="card__button card__button--email">
+              <a href={`https://${ user.github }`}>
+                <i className="fas fa-envelope fa-3x"></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </NavLink>
   );
 };
 
