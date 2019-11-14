7d52cdb8fdf3216d139eff17d8a67e0132d8e961
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-11-13 22:36:05 -0500

    base styling on send messages component

--- client/src/components/pages/SendMessage.js
+++ client/src/components/pages/SendMessage.js
@@ -24,16 +24,19 @@ const SendMessage = () => {
 
   return(
     <Container>
-      <h1>Send a message</h1>
-      <form onSubmit={e => handleSubmit(e)}>
-        <h2>Recipient</h2>
-        <input type="text" onChange={e => setRecipient(e.target.value)} />
-        <h2>Subject</h2>
-        <input type="text" onChange={e => setSubject(e.target.value)} />
-        <h2>Message</h2>
-        <input type="text" onChange={e => setMessage(e.target.value)}/>
-        <button type="submit">send</button>
-      </form>
+      <Form>
+        <Header>Send a message</Header>
+        <form onSubmit={e => handleSubmit(e)}>
+          <Label>Recipient</Label>
+          <Input type="text" onChange={e => setRecipient(e.target.value)} />
+          <Label>Subject</Label>
+          <Input type="text" onChange={e => setSubject(e.target.value)} />
+          <Label>Message</Label>
+          <TextArea type="text" onChange={e => setMessage(e.target.value)}/>
+
+          <Button type="submit">send</Button>
+        </form>
+      </Form>
     </Container>
   );
 }
@@ -44,4 +47,47 @@ const Container = styled.div`
   padding-top: 10rem;
   border: 1px solid red;
   height: 95vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Form = styled.div`
+  padding: 2rem 3rem;
+  border: 1px solid black;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`
+
+const Header = styled.h1`
+  font-size: 3rem;
+  margin-bottom: 2rem;
+`
+
+const Label = styled.h2`
+  font-size: 2rem;
+  margin: 1rem 0 1rem 0;
+`
+
+const Input = styled.input`
+  width: 80%;
+  height: 2.7rem;
+  border: 1px solid black;
+`
+
+const TextArea = styled.textarea`
+  width: 50rem;
+  height: 25rem;
+`
+
+const Button = styled.div`
+  padding: 1rem 1.5rem;
+  font-size: 1.5rem;
+  border: 1px solid black;
+  width: 60%;
+  margin: 2rem auto 0;
 `
